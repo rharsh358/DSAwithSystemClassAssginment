10cae224def1cf3d03c48174b6acc05c8432c802
Harshad <harshadramteke358@gmail.com>
2024-12-18 18:20:50 +0530

    18 dec twosum Leetcode

---
+++ src/practice/dsa/TestClass.java
@@ -0,0 +1,11 @@
+package practice.dsa;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+
+        //[2,7,11,15]     [3,2,4]       [3,3]
+        //9                 6            6
+    }
+}
---
+++ src/practice/dsa/TwoSum.java
@@ -0,0 +1,61 @@
+package practice.dsa;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4};
+        int target = 6;
+        int n = arr.length;
+        int[] abc = twoSum(arr, target);
+
+        System.out.println(" Print Array " + Arrays.toString(abc));
+    }
+
+
+    // This is for getting indexes
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        if (n < 2) {
+            System.out.println("Invalid Input");
+            return new int[0];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+
+                }
+            }
+        }
+        return new int[0];
+    }
+
+
+    // This is for getting actaul values
+    public static int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        if (n < 2) {
+            System.out.println("Invalid Input");
+            return new int[0];
+        }
+        int currentValue = 0;
+        int previousValue = 0;
+        for (int i = 0; i < n; i++) {    //3, 2, 4
+
+
+            int temp = nums[i];  // temp = 3,2, 4
+            previousValue = currentValue; //  3 2
+            currentValue = temp;  // curr valu =3 ,2 , 4
+
+            if (currentValue + previousValue == target) {
+                return new int[]{previousValue, currentValue};
+            }
+
+        }
+        return new int[0];
+    }
+
+
+}

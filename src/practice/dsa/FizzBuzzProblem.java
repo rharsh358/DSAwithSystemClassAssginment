d4cd1ca060f763ccbae7fd72a00817428907e66e
Harshad <harshadramteke358@gmail.com>
2025-01-10 12:08:18 +0530

    19 dec Pascel O base Traingle

---
+++ src/practice/dsa/FizzBuzzProblem.java
@@ -0,0 +1,22 @@
+package practice.dsa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FizzBuzzProblem {
+
+    public static void main(String[] args) {
+        List<String> str = Collections.singletonList("FizBuzzz");
+        int n = str.toArray().length;
+        fizzBuzz(n);
+
+    }
+
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> str = new ArrayList<>();
+
+        return str;
+    }
+}
